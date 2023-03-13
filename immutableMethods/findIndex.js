b0f2e7e20d5917085d75df24edf6b224f2afc452
Vache Apinyan <apinyan.v@gmail.com>
2023-03-13 22:12:54 +0400

    fix(findIndex) fixed bug in code.

--- immutableMethods/findIndex.js
+++ immutableMethods/findIndex.js
@@ -2,14 +2,11 @@
 
 function myFindIndex(array, element) {
     for(let i = 0; i < array.length; i++) {
-        let item = array[i];      
-        if(item === element) {
-            return `The number ${element} in the array index is ${i}`
-        }
-        if(i === array.length-1) {
-            return `${element} is not in array`
+        if(array[i] === element) {
+            return i;
         }
     }
-}
+    return -1;
+};
 const array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 console.log(myFindIndex(array, 5))
