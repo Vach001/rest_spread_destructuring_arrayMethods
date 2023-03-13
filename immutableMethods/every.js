51394c43b22b38e2cf3009269f882c20377206a5
Vache Apinyan <apinyan.v@gmail.com>
2023-03-13 23:37:03 +0400

    fix(every): fixed bug in every function code.

--- immutableMethods/every.js
+++ immutableMethods/every.js
@@ -1,14 +1,16 @@
 //implementation every function method
 
-function myEvery(myArray, action = () => {}) {
-    let isEvery = true
-
+function myEvery(myArray = [], action = () => {}) {
     for(let i = 0; i <myArray.length; i++) {
-        let currentItem = myArray[i] 
-        
-        if(!action(currentItem)) {
-            return isEvery = false
+        if(!action(myArray[i], i, myArray)) {
+            return false;
         } 
     }
-    return isEvery;
+    return true;
 };
+
+const arr = [5, 3, 9, 11];
+const foo = ((item) => item % 2 !==0);
+const odd = myEvery(arr, foo);
+
+console.log(odd);
