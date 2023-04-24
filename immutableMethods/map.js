33ae85a8aae55ad98c1df3538024abd1bdbf0b51
Vache Apinyan <apinyan.v@gmail.com>
2023-04-24 20:50:12 +0400

    feat(map): new implementation map

--- immutableMethods/map.js
+++ immutableMethods/map.js
@@ -1,13 +1,22 @@
 //implementation map function method
 
-function myMap(array) {
-    let newArray = [];
+Array.prototype.myMap = function (action = () => {}) {
+  let newArray = [];
 
-    for(let i = 0; i < array.length; i++) {
-        newArray.push(array[i]);
-    }
-    return newArray;
+  for (let i = 0; i < this.length; i++) {
+    newArray.push(action(this[i], i, this));
+  }
+  return newArray;
 };
 
-const example = [1, 11, 2, 22, 3, 33, 333];
-console.log(myMap(example));   
+const array = [1, 11, 2, 22, 3, 33, 333];
+const foo = (item) => item * 3;
+const checkMyMap = array.myMap(foo);
+
+console.log(checkMyMap);
+
+//check implementation
+
+const checkMap = array.map(foo);
+
+console.log(checkMap);
