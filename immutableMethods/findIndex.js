ca3d5c4d24151a2ed8487daaea31ab7c3d08e3c2
Vache Apinyan <apinyan.v@gmail.com>
2023-04-24 19:34:47 +0400

    feat(findIndex): new implementation findIndex

--- immutableMethods/findIndex.js
+++ immutableMethods/findIndex.js
@@ -1,12 +1,24 @@
 //implementation findIndex function method
 
-function myFindIndex(array, element) {
-    for(let i = 0; i < array.length; i++) {
-        if(array[i] === element) {
-            return i;
-        }
+Array.prototype.myFindIndex = function (action = () => {}) {
+  for (let i = 0; i < this.length; i++) {
+    const element = this[i];
+    const result = action(element);
+
+    if (result) {
+      return i;
     }
-    return -1;
+  }
+  return -1;
 };
-const array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-console.log(myFindIndex(array, 5))
+
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const foo = (item) => item === 5;
+
+const checkMyFindIndex = array.myFindIndex(foo);
+console.log(checkMyFindIndex);
+
+//checking implementation
+
+const check = array.findIndex(foo);
+console.log(check);
