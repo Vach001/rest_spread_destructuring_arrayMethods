95479037c650c242e7b31c6cbacf35d6d2e91b4b
Vache Apinyan <apinyan.v@gmail.com>
2023-04-24 20:26:55 +0400

    feat(forEach): new implementation forEach

--- immutableMethods/forEach.js
+++ immutableMethods/forEach.js
@@ -1,11 +1,18 @@
-//implementation / Array.forEach() 
+//implementation / Array.forEach()
 
-function forEach(array) {
-    for(let i = 0; i < array.length; i++) {
-
-        console.log(arr[i]);
-    }
+Array.prototype.myForEach = function (action = () => {}) {
+  for (let i = 0; i < this.length; i++) {
+    action(this[i], i, this);
+  }
 };
 
-const arr = [7,8,9,"yes", true, undefined, , "NO", ' '];   // empty slots isn't working
-console.log(forEach(arr));
+const arr = [7, 8, 9, "yes", true, undefined]; // empty slots isn't working
+const foo = (item) => console.log(item);
+
+const checkMyForEach = arr.myForEach(foo);
+console.log(checkMyForEach);
+
+// check implementation
+
+const check = arr.forEach(foo);
+console.log(check);
