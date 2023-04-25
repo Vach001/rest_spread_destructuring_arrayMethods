1c749cacff02ab45f366ca0c7c172a4f32b284d8
Vache Apinyan <apinyan.v@gmail.com>
2023-04-25 13:13:14 +0400

    feat(filter): change implementation to filter

--- immutableMethods/filter.js
+++ immutableMethods/filter.js
@@ -4,17 +4,16 @@ Array.prototype.myFilter = function (action = () => {}) {
   const newArray = [];
 
   for (let i = 0; i < this.length; i++) {
-    const currentItem = this[i];
 
-    if (action(currentItem)) {
-      newArray.push(currentItem);
+    if (action(this[i], i, this)) {
+      newArray.push(this[i]);
     }
   }
   return newArray;
 };
 
 const myArray = [9, 8, 7, 6, 5, 4, 3];
-const foo = (item) => item > 5;
+const foo = (item) => item >= 5;
 
 const checkMyArray = myArray.myFilter(foo);
 console.log(checkMyArray);
